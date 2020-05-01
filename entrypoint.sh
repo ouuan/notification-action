49b264ec0ff0963b876f58e64e1916e3e55cb50b
ouuan <ouuansteve@gmail.com>
2020-05-01 20:33:54 +0800

    feat: Add format option

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-MESSAGE=$(python /main.py "$1" "$2")
+MESSAGE=$(python /main.py "$1" "$2" "$3")
 
 # escape characters
 # https://github.community/t5/GitHub-Actions/set-output-Truncates-Multiline-Strings/m-p/38372#M3322
