2ee3210792f9034a49c6049f0f2115814b0333a6
ouuan <ouuansteve@gmail.com>
2020-05-02 20:19:49 +0800

    refactor: Use formatter to deal with different formats

--- events/push.py
+++ events/push.py
@@ -1,50 +1,29 @@
 import json
+import events.formatter as fmt
 
 
-def message(e, messageFormat):
-    if messageFormat == 'markdown':
-        commits = e['commits']
-        forced = bool(e['forced'])
-        compareUrl = e['compare']
-        repoName = e['repository']['full_name']
-        repoUrl = e['repository']['html_url']
-        pusherName = e['pusher']['name']
-        ref = e['ref']
-        if ref[:11] == 'refs/heads/':
-            ref = '[{0}]({1}/tree/{0})'.format(ref[11:], repoUrl)
-        elif ref[:10] == 'refs/tags/':
-            ref = '[{0}]({1}/releases/tag/{0})'.format(ref[10:], repoUrl)
-        message = '{} {}pushed [{} commit{}]({}) to [{}]({}):{}:\n\n'.format(
-            pusherName, 'forced-' if forced else '', len(commits), 's' if len(commits) >= 2 else '', compareUrl, repoName, repoUrl, ref)
-        for commit in commits:
-            shortSHA = commit['id'][:7]
-            url = commit['url']
-            author = commit['author']['name']
-            commitMessage = commit['message']
-            message += '[{}]({}) - {} by.{}  \n'.format(shortSHA,
-                                                        url, commitMessage, author)
-        return message
-
-    elif messageFormat == 'plaintext':
-        commits = e['commits']
-        forced = bool(e['forced'])
-        repoName = e['repository']['full_name']
-        pusherName = e['pusher']['name']
-        ref = e['ref']
-        if ref[:11] == 'refs/heads/':
-            ref = '{}'.format(ref[11:])
-        elif ref[:10] == 'refs/tags/':
-            ref = '{}'.format(ref[10:])
-        message = '{} {}pushed {} commit{} to {}:{}:\n\n'.format(
-            pusherName, 'forced-' if forced else '', len(commits), 's' if len(commits) >= 2 else '', repoName, ref)
-        for commit in commits:
-            shortSHA = commit['id'][:7]
-            author = commit['author']['name']
-            commitMessage = commit['message']
-            message += '{} - {} by.{}\n'.format(shortSHA,
-                                                commitMessage, author)
-        return message
-
-    else:
-        raise Exception(
-            'The format {} is unsupported for the event push'.format(format))
+def message(e):
+    commits = e['commits']
+    forced = bool(e['forced'])
+    compareUrl = e['compare']
+    repoName = e['repository']['full_name']
+    repoUrl = e['repository']['html_url']
+    pusherName = e['pusher']['name']
+    ref = e['ref']
+    if ref[:11] == 'refs/heads/':
+        branch = ref[11:]
+        ref = fmt.link(
+            branch, '{}/tree/{}'.format(repoUrl, branch))
+    elif ref[:10] == 'refs/tags/':
+        tag = ref[10:]
+        ref = fmt.link(tag, '{}/releases/tag/{}'.format(repoUrl, tag))
+    message = fmt.paragraph('{} {}pushed {} to {}:{}:'.format(
+        pusherName, 'forced-' if forced else '', fmt.link('{} commit{}'.format(len(commits), 's' if len(commits) >= 2 else ''), compareUrl), fmt.link(repoName, repoUrl), ref))
+    for commit in commits:
+        shortSHA = commit['id'][:7]
+        url = commit['url']
+        author = commit['author']['name']
+        commitMessage = commit['message']
+        message += '{} - {} by.{}{}'.format(
+            fmt.link(shortSHA, url), commitMessage, author, fmt.br())
+    return message
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import sys
 import json
 
+import events.formatter
 import events.push
 
 if __name__ == "__main__":
     if (len(sys.argv) != 4):
-        print('python main.py <format> <event-type> <event-json>')
+        print('python main.py <event-name> <event-json> <format>')
         raise Exception(
             'The number of arguments is {}, expected 3'.format(len(sys.argv) - 1))
 
-    messageFormat = sys.argv[1]
-    eventType = sys.argv[2]
-    eventJson = json.loads(sys.argv[3])
+    eventName = sys.argv[1]
+    eventJson = json.loads(sys.argv[2])
+    messageFormat = sys.argv[3]
 
-    if eventType == 'push':
-        print(events.push.message(eventJson, messageFormat))
+    events.formatter.setFormat(messageFormat)
+
+    if eventName == 'push':
+        print(events.push.message(eventJson))
     else:
-        raise Exception('The event type {} is unsupported'.format(eventType))
+        raise Exception('The event "{}" is unsupported'.format(eventName))
