77a37acbd53e5acb1ef0d61d45c88e8d74de68f8
Yufan You <ouuansteve@gmail.com>
2022-12-05 01:27:02 +0800

    fix: use env instead of args to pass inputs
    "test"

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-message="$(python /main.py "$1" "$2" "$3")"
+message="$(python /main.py "$EVENT_NAME" "$EVENT_JSON" "$FORMAT")"
 delimiter="$(tr -dc A-Za-z </dev/urandom | head -c 50)"
 
 echo "message<<$delimiter
