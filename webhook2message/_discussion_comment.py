7c448436a3d5665ae85dbf8b74a5c5d0e4fabcc3
mgt <mgt@oi-wiki.org>
2022-12-05 00:11:50 +0800

    fix: typo and add repo

--- webhook2message/_discussion_comment.py
+++ webhook2message/_discussion_comment.py
@@ -5,8 +5,11 @@ def message(e):
     action = e['action']
     comment = e['comment']
     discussion = e['discussion']
+    repo = e['repository']
+    title_repo =  p.link(f"{repo['name']}#{discussion['number']}", discussion['html_url'])
+    title_comment = p.link(discussion['title'], comment['html_url'])
     if action == 'created':
-        return p.paragraph(f"New comment on {p.link(discussion['title'], comment['html_url'])} by {p.link(comment['user']['login'], comment['user']['html_url'])}") + comment['body']
+        return p.paragraph(f"New comment on {title_repo} {title_comment} by {p.link(comment['user']['login'], comment['user']['html_url'])}") + comment['body']
     else:
         raise Exception(
-            f'The action {action} in the pull_request event is not supported.')
+            f'The action {action} in the discussion_comment event is not supported.')
