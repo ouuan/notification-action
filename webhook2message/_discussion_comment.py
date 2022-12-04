5686a3ae2362356257c3b4f5b7127b2bd9bf178c
mgt <mgt@oi-wiki.org>
2022-12-05 00:30:53 +0800

    match the title style

--- webhook2message/_discussion_comment.py
+++ webhook2message/_discussion_comment.py
@@ -7,13 +7,11 @@ def message(e):
     discussion = e['discussion']
     repo = e['repository']
 
-    comment_link = p.link('New comment', comment['html_url'])
-    repo_title =  p.link(f"{repo['name']}#{discussion['number']}", discussion['html_url'])
-    discussion_title = discussion['title']
+    title =  p.link(f"{repo['name']}#{discussion['number']} {discussion['title']}", comment['html_url'])
     user_link = p.link(comment['user']['login'], comment['user']['html_url'])
 
     if action == 'created':
-        return p.paragraph(f"{comment_link} on {repo_title} {discussion_title} by {user_link}") + comment['body']
+        return p.paragraph(f"New comment on {title} by {user_link}") + comment['body']
     else:
         raise Exception(
             f'The action {action} in the discussion_comment event is not supported.')
