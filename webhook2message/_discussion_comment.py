6d029c91e239de73b345a0bc03ac22857409bb1f
mgt <mgt@oi-wiki.org>
2022-12-04 23:51:32 +0800

    add discussion comment

---
+++ webhook2message/_discussion_comment.py
@@ -0,0 +1,12 @@
+import webhook2message._partials as p
+
+
+def message(e):
+    action = e['action']
+    comment = e['comment']
+    discussion = e['discussion']
+    if action == 'created':
+        return p.paragraph(f"New comment on {p.link(discussion['title'], comment['html_url'])} by {p.link(comment['user']['login'], comment['user']['html_url'])}") + p.paragraph(comment['body'])
+    else:
+        raise Exception(
+            f'The action {action} in the pull_request event is not supported.')
