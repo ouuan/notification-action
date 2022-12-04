e870e0ec50daf3b3284897a163feb69dd951dc4b
mgt <mgt@oi-wiki.org>
2022-12-04 23:53:45 +0800

    chore: remove use of  paragraph for body

--- webhook2message/_discussion_comment.py
+++ webhook2message/_discussion_comment.py
@@ -6,7 +6,7 @@ def message(e):
     comment = e['comment']
     discussion = e['discussion']
     if action == 'created':
-        return p.paragraph(f"New comment on {p.link(discussion['title'], comment['html_url'])} by {p.link(comment['user']['login'], comment['user']['html_url'])}") + p.paragraph(comment['body'])
+        return p.paragraph(f"New comment on {p.link(discussion['title'], comment['html_url'])} by {p.link(comment['user']['login'], comment['user']['html_url'])}") + comment['body']
     else:
         raise Exception(
             f'The action {action} in the pull_request event is not supported.')
